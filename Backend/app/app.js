3d73b84af50c7f75a6c8656e966156e65a2a2886
Henriquecandls <henriqueccandelaria@gmail.com>
2026-05-28 16:14:07 +0100

    added console log for env debug

--- Backend/app/app.js
+++ Backend/app/app.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_HOST:", process.env.DB_HOST);
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
